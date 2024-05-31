9ee79ebb7e7cdcd46e47afe0e610c08387c2a3bf
Pengshimin <1760967618@qq.com>
2024-05-31 12:01:38 +0800

    fix

--- generateSidebar.js
+++ generateSidebar.js
@@ -1,44 +1,46 @@
-import { readdirSync, statSync } from "fs";
-import { join, extname, basename } from "path";
+import { readdirSync, statSync } from 'fs';
+import { join, extname, basename } from 'path';
 
 function generateNotes(fullPath, relativePath = '') {
-  const notes = [];
-  const items = readdirSync(fullPath);
+	const notes = [];
+	const items = readdirSync(fullPath);
 
-  items.forEach(item => {
-    const itemFullPath = join(fullPath, item);
-    const itemRelativePath = join(relativePath, item);
-    const stats = statSync(itemFullPath);
+	items.forEach((item) => {
+		const itemFullPath = join(fullPath, item);
+		const itemRelativePath = join(relativePath, item);
+		const stats = statSync(itemFullPath);
 
-    if (stats.isDirectory()) {
-      const children = generateNotes(itemFullPath, itemRelativePath);
-      const mdFiles = children.filter(child => typeof child === 'string');
-      if (mdFiles.length > 0) {
-        notes.push({
-          dir: itemRelativePath.replace(/\\/g, "/"),
-          link: '/notes/' + itemRelativePath.replace(/\\/g, "/") + '/',
-          sidebar: [{
-            text: basename(item),
-            collapsed: true,
-            items: mdFiles
-          }]
-        });
-      }
-      const directories = children.filter(child => typeof child === 'object');
-      notes.push(...directories);
-    } else if (stats.isFile() && extname(item) === '.md') {
-      notes.push(basename(item, '.md'));
-    }
-  });
+		if (stats.isDirectory()) {
+			const children = generateNotes(itemFullPath, itemRelativePath);
+			const mdFiles = children.filter((child) => typeof child === 'string');
+			if (mdFiles.length > 0) {
+				notes.push({
+					dir: itemRelativePath.replace(/\\/g, '/'),
+					link: '/notes/' + itemRelativePath.replace(/\\/g, '/') + '/',
+					sidebar: [
+						{
+							text: basename(item),
+							collapsed: true,
+							items: mdFiles,
+						},
+					],
+				});
+			}
+			const directories = children.filter((child) => typeof child === 'object');
+			notes.push(...directories);
+		} else if (stats.isFile() && extname(item) === '.md') {
+			notes.push(basename(item, '.md'));
+		}
+	});
 
-  return notes;
+	return notes;
 }
 
 const notes = {
-  dir: "/notes",
-  link: "/",
-  notes: generateNotes("./docs/notes"),
+	dir: '/notes',
+	link: '/',
+	notes: generateNotes('./docs/notes'),
 };
 
 export { notes };
-console.log(JSON.stringify(notes, null, 2));
+// console.log(JSON.stringify(notes, null, 2));
